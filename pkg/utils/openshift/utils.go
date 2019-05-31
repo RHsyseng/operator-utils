e719effe6e4098b25f6d67030d76636fda819192
Jeremy Ary <jeremy.ary@gmail.com>
2019-05-31 15:08:43 -0500

    trying newer che logic

--- pkg/utils/openshift/utils.go
+++ pkg/utils/openshift/utils.go
@@ -1,62 +1,40 @@
 package openshift
 
 import (
-	"encoding/json"
-
-	log "k8s.io/klog"
-	api "k8s.io/kubernetes/pkg/api/unversioned"
-	clientset "k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset"
-	restclient "k8s.io/kubernetes/pkg/client/restclient"
-	cmdutil "k8s.io/kubernetes/pkg/kubectl/cmd/util"
+	"k8s.io/client-go/discovery"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
+	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
 )
 
-type MasterType string
+//type MasterType string
+//
+//const (
+//	OpenShift  MasterType = "OpenShift"
+//	Kubernetes MasterType = "Kubernetes"
+//)
 
-const (
-	OpenShift  MasterType = "OpenShift"
-	Kubernetes MasterType = "Kubernetes"
-)
+var log = logf.Log.WithName("env")
 
 func IsOpenShift() (bool, error) {
-
-	client, _ := NewClient(cmdutil.NewFactory(nil))
-	typeOfMaster := TypeOfMaster(client)
-
-	return typeOfMaster == OpenShift, nil
-}
-
-func TypeOfMaster(c *clientset.Clientset) MasterType {
-	res, err := c.CoreClient.RESTClient().Get().AbsPath("").DoRaw()
+	kubeconfig, err := config.GetConfig()
 	if err != nil {
-		log.Fatalf("Could not discover the type of your installation: %v", err)
+		return false, err
 	}
-
-	var rp api.RootPaths
-	err = json.Unmarshal(res, &rp)
+	discoveryClient, err := discovery.NewDiscoveryClientForConfig(kubeconfig)
 	if err != nil {
-		log.Fatalf("Could not discover the type of your installation: %v", err)
+		return false, err
 	}
-	for _, p := range rp.Paths {
-		if p == "/oapi" {
-			return OpenShift
+	apiList, err := discoveryClient.ServerGroups()
+	if err != nil {
+		return false, err
+	}
+	apiGroups := apiList.Groups
+	log.Info("In IsOpenshift", "apiGroups", apiGroups)
+	for i := 0; i < len(apiGroups); i++ {
+		if apiGroups[i].Name == "route.openshift.io" {
+			log.Info("In IsOpenshift => returning true, nil")
+			return true, nil
 		}
 	}
-	return Kubernetes
-}
-
-func NewClient(f cmdutil.Factory) (*clientset.Clientset, *restclient.Config) {
-	var err error
-
-	cfg, err := f.ClientConfig()
-	if err != nil {
-		log.Error("Could not initialise a client - is your server setting correct?\n\n")
-		log.Fatalf("%v", err)
-	}
-
-	c, err := clientset.NewForConfig(cfg)
-	if err != nil {
-		log.Fatalf("Could not initialise a client: %v", err)
-	}
-
-	return c, cfg
+	return false, nil
 }
