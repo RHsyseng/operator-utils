ae8484e342a0c12b65815a20c11b331a9b060e8a
Jeremy Ary <jeremy.ary@gmail.com>
2019-06-27 07:50:09 -0500

    pulling OpenShiftVersion call/info into own func calls

--- internal/platform/platform_versioner_test.go
+++ internal/platform/platform_versioner_test.go
@@ -59,65 +59,6 @@ func (pv FakePlatformVersioner) GetPlatformInfo(d Discoverer, cfg *rest.Config) 
 	return pv.Info, nil
 }
 
-func TestDetectOpenShift(t *testing.T) {
-
-	ocpInfo := PlatformInfo{
-		Name:       OpenShift,
-		OCPVersion: "1.2.3",
-		K8SVersion: "4.5.6",
-		OS:         "foo/bar",
-	}
-	k8sInfo := ocpInfo
-	k8sInfo.Name = Kubernetes
-
-	cases := []struct {
-		pv           FakePlatformVersioner
-		cfg          *rest.Config
-		expectedBool bool
-		expectedErr  bool
-		label        string
-	}{
-		{
-			label: "case 1", // return OCP info, ensure true with no error
-			pv: FakePlatformVersioner{
-				Info: ocpInfo,
-				Err:  nil,
-			},
-			cfg:          &rest.Config{},
-			expectedBool: true,
-			expectedErr:  false,
-		},
-		{
-			label: "case 2", // return k8s info, ensure false with no error
-			pv: FakePlatformVersioner{
-				Info: k8sInfo,
-				Err:  nil,
-			},
-			cfg:          &rest.Config{},
-			expectedBool: false,
-			expectedErr:  false,
-		},
-		{
-			label: "case 3", // trigger error, should return alongside false
-			pv: FakePlatformVersioner{
-				Info: ocpInfo,
-				Err:  errors.New("uh oh"),
-			},
-			cfg:          &rest.Config{},
-			expectedBool: false,
-			expectedErr:  true,
-		},
-	}
-
-	for _, c := range cases {
-		IsOpenShift, err := DetectOpenShift(c.pv, c.cfg)
-		assert.Equal(t, c.expectedBool, IsOpenShift, c.label+": mismatch in returned boolean result")
-		if c.expectedErr {
-			assert.Error(t, err, c.label+": expected error, but none occurred")
-		}
-	}
-}
-
 func TestK8SBasedPlatformVersioner_GetPlatformInfo(t *testing.T) {
 
 	pv := K8SBasedPlatformVersioner{}
@@ -169,63 +110,19 @@ func TestK8SBasedPlatformVersioner_GetPlatformInfo(t *testing.T) {
 			expectedErr:  false,
 		},
 		{
-			label: "case 4", // let K8S version start with "1.10+", info should now reflect OCP approximation
+			label: "case 4", // trigger no errors, simulate OCP route present
 			discoverer: FakeDiscoverer{
 				serverInfo: &version.Info{
 					Major: "1",
-					Minor: "10+",
+					Minor: "2",
 				},
 				groupList: &v1.APIGroupList{
 					TypeMeta: v1.TypeMeta{},
-					Groups: []v1.APIGroup{
-						{
-							Name: "route.openshift.io",
-						},
-					},
+					Groups:   []v1.APIGroup{{Name: "route.openshift.io"}},
 				},
 			},
 			config:       &rest.Config{},
-			expectedInfo: PlatformInfo{Name: OpenShift, K8SVersion: "1.10+", OCPVersion: "3.10"},
-			expectedErr:  false,
-		},
-		{
-			label: "case 5", // let K8S version start with "1.11+", info should now reflect OCP approximation
-			discoverer: FakeDiscoverer{
-				serverInfo: &version.Info{
-					Major: "1",
-					Minor: "11+",
-				},
-				groupList: &v1.APIGroupList{
-					TypeMeta: v1.TypeMeta{},
-					Groups: []v1.APIGroup{
-						{
-							Name: "route.openshift.io",
-						},
-					},
-				},
-			},
-			config:       &rest.Config{},
-			expectedInfo: PlatformInfo{Name: OpenShift, K8SVersion: "1.11+", OCPVersion: "3.11"},
-			expectedErr:  false,
-		},
-		{
-			label: "case 6", // let K8S version start with "1.13+", info should now reflect OCP approximation
-			discoverer: FakeDiscoverer{
-				serverInfo: &version.Info{
-					Major: "1",
-					Minor: "13+",
-				},
-				groupList: &v1.APIGroupList{
-					TypeMeta: v1.TypeMeta{},
-					Groups: []v1.APIGroup{
-						{
-							Name: "route.openshift.io",
-						},
-					},
-				},
-			},
-			config:       &rest.Config{},
-			expectedInfo: PlatformInfo{Name: OpenShift, K8SVersion: "1.13+", OCPVersion: "4.1"},
+			expectedInfo: PlatformInfo{Name: OpenShift, K8SVersion: "1.2"},
 			expectedErr:  false,
 		},
 	}
